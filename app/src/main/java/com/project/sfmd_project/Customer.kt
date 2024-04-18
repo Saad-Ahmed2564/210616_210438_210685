b00a90ebce63e66372870483cb68295e4bbbb3a5
saad bilal <saadbilal1722@gmail.com>
2024-04-18 20:58:12 +0500

    Initial commit

---
+++ app/src/main/java/com/project/sfmd_project/Customer.kt
@@ -0,0 +1,28 @@
+package com.project.sfmd_project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+
+class Customer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val layoutParams = WindowManager.LayoutParams().apply {
+            flags = WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        }
+        window.attributes = layoutParams
+
+        setContentView(R.layout.activity_customer)
+
+        val back = findViewById<Button>(R.id.back)
+        back.setOnClickListener {
+            val intent = Intent(this@Customer, Home::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
